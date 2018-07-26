9a77cd4c860de98d9250f0bdbf6cf516d72aac10
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-26 19:05:23 +0530

    Add files via upload

---
+++ JarTest/src/EmpTest.java
@@ -0,0 +1,9 @@
+//Jar File Test
+public class EmpTest {
+public static void main(String args[])
+{
+	Employee e=new Employee("Arijit","124367",25000);
+	e.display();
+			
+}
+}
---
+++ OrgTest/src/OrgTest.java
@@ -0,0 +1,9 @@
+import Pack2.*;
+public class OrgTest {
+private static void main(String args[]) {
+	Org o=new Org();
+	
+	
+	
+}
+}
